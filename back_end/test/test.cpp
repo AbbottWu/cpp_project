c87d3aba02727a315f2b27aa939b3a5ca4862b55
jiajiansheng <1742055050@qq.com>
2022-08-21 21:40:31 +0800

    try

--- back_end/test/test.cpp
+++ back_end/test/test.cpp
@@ -3,11 +3,13 @@
 #include "../src/user.h"
 #include "../src/question.h"
 #include "../src/data_engine.h"
-
+//单元测试
+//第一个是测试用例名，一组小测试的组合名；第二个是测试名
+//用来保证每节的代码是ok的
 TEST(UserTest, BasicTest) {
 	// 空元素测试
 	User user;
-	EXPECT_EQ(user.get_name(), "");
+	EXPECT_EQ(user.get_name(), "");//EQ 等于
 	EXPECT_EQ(user.get_token(), "");
 	EXPECT_TRUE(user.get_isAnswer());
 	EXPECT_TRUE(typeid(user.get_questions()) == typeid(vector<Question*>));
