b1a9b946ed18ce25a777b22519a3c53308a83e73
AbbottWu <abbottdescriber@outlook.com>
2022-08-29 14:43:15 +0800

    追加实现CPP的GRPC服务端函数

--- back_end/src/grpc.cpp
+++ back_end/src/grpc.cpp
@@ -1,35 +1,63 @@
 #include "grpc.h"
 
+
 User *create_tmp_user_from_proto(const app::User *proto_user) {
     User *temp_user = new User(proto_user->name(), proto_user->token(), proto_user->is_answerer());
     return temp_user;
 }
 
-grpc::Status RouteImpl::SignUp(grpc::ServerContext *context, const app::User *input, app::IsOK *output) {
-    cout << "SignUp called" << endl;
+grpc::Status RouteImpl::SignUp(grpc::ServerContext *, const app::User *input, app::IsOK *output) {
+    cout << "SignUp Called" << endl;
     User *temp_user = create_tmp_user_from_proto(input);
     App &runtime = App::get_instance(nullptr);
-    bool local_status = runtime.local_register(temp_user);
-    if (!local_status) {
-        cout << "SignUp failed" << endl;
-        output->set_success(false);
-        return grpc::Status::CANCELLED;
-    }
-    cout << "SignUp success" << endl;
+    bool result = runtime.local_register(temp_user);
+    if (result) {
+        cout << "SignUp Success" << endl;
         output->set_success(true);
+    }
+    cout << "SignUp Failed" << endl;
+    output->set_success(false);
+    delete temp_user;
     return grpc::Status::OK;
 }
 
 
-grpc::Status RouteImpl::Login(grpc::ServerContext *, const app::User *, app::IsOK *) {
+grpc::Status RouteImpl::Login(grpc::ServerContext *, const app::User *input, app::IsOK *output) {
+    cout << "Login Called" << endl;
+    App &runtime = App::get_instance(nullptr);
+    User *temp_user = create_tmp_user_from_proto(input);
+    bool result = runtime.local_login(temp_user);
+    if (result) {
+        output->set_success(true);
+        cout << "Login Success" << endl;
+    }
+    output->set_success(false);
+    cout << "Login Failed" << endl;
+    delete temp_user;
     return grpc::Status::OK;
 }
 
-grpc::Status RouteImpl::AllQuestions(grpc::ServerContext *, const app::RequestQuestions *, app::Questions *) {
+grpc::Status
+RouteImpl::AllQuestions(grpc::ServerContext *, const app::RequestQuestions *input, app::Questions *output) {
+    cout << "All Questions Called" << endl;
+    App &runtime = App::get_instance(nullptr);
+    auto temp_questions = runtime.all_questions(input->is_answered());
+    for (auto question: temp_questions) {
+        auto one_question = output->add_questions();
+        one_question->set_title(question->get_title());
+        one_question->set_content(question->get_content());
+        one_question->set_id(question->get_id());
+        one_question->set_category(question->get_category());
+        one_question->set_answered(question->is_answered());
+        one_question->set_answer(question->get_answer());
+    }
+    cout << "All Questions Success" << endl;
     return grpc::Status::OK;
 }
 
-grpc::Status RouteImpl::MyQuestions(grpc::ServerContext *, const app::RequestQuestions *, app::Questions *) {
+grpc::Status RouteImpl::MyQuestions(grpc::ServerContext *, const app::RequestQuestions *input, app::Questions *output) {
+    cout << "My Questions Called" << endl;
+    App &runtime = App::get_instance(nullptr);
     return grpc::Status::OK;
 }
 
